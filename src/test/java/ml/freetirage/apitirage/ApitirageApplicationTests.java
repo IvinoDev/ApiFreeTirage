9c885d41c1ac22b6eeeae52c977c0c7afdca6995
IvinoDev <kayantao@protonmail.com>
2022-08-17 17:23:13 +0000

    Création du projet

---
+++ src/test/java/ml/freetirage/apitirage/ApitirageApplicationTests.java
@@ -0,0 +1,13 @@
+package ml.freetirage.apitirage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApitirageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
